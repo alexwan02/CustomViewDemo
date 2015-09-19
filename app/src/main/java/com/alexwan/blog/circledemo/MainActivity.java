427fa579e1833813773b38a543c7782668e74666
alexwan1989 <alexwan1989@sina.com>
2015-09-19 10:28:29 +0800

    no message

--- app/src/main/java/com/alexwan/blog/circledemo/MainActivity.java
+++ app/src/main/java/com/alexwan/blog/circledemo/MainActivity.java
@@ -1,53 +1,36 @@
 package com.alexwan.blog.circledemo;
 
-import android.animation.ObjectAnimator;
-import android.graphics.Typeface;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextPaint;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.TextView;
 
-import com.alexwan.blog.circledemo.widget.CircleView;
+import com.alexwan.blog.circledemo.widget.CustomView;
+
 
 public class MainActivity extends AppCompatActivity {
-    private CircleView circleView;
+    private CustomView customView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        circleView = (CircleView) findViewById(R.id.circle);
-
-//        ObjectAnimator animator = ObjectAnimator.ofFloat(circleView, "rotation", 0f, 360f);
-//        animator.setInterpolator(new LinearInterpolator());
-//        animator.setDuration(1000);
-//        animator.setRepeatCount(-1);
-//        animator.start();
-//        new Thread(circleView).start();
-//        TextView tv = (TextView) findViewById(R.id.text_view);
-//        Typeface typeface = Typeface.createFromAsset(this.getAssets(),"font/NotoSans-Italic.ttf");
-//        TextPaint paint = tv.getPaint();
-//        paint.setStrikeThruText(true);
-//        paint.setTypeface(typeface);
+        customView = (CustomView) findViewById(R.id.custom_view);
 //        handler.sendEmptyMessage(1);
-//        handler.post(new Thread(circleView){
-//            @Override
-//            public void run() {
-//                super.run();
-//
-//            }
-//        });
-    }
-//    private Handler handler = new Handler(){
-//        @Override
-//        public void handleMessage(Message msg) {
-//            super.handleMessage(msg);
-////            new Thread(circleView).start();
-//        }
-//    };
 
+    }
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            // 每500毫秒发送一个消息
+//            handler.sendEmptyMessageDelayed(1 , 500);
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // activity销毁的时候清除handler的引用
+        handler.removeCallbacksAndMessages(null);
+    }
 }
--- app/src/main/java/com/alexwan/blog/circledemo/widget/CustomView.java
+++ app/src/main/java/com/alexwan/blog/circledemo/widget/CustomView.java
@@ -5,11 +5,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 
 /**
  * Created by alexwan on 15/9/14.
@@ -17,7 +18,7 @@ import android.view.Window;
 public class CustomView extends View {
     private Paint paint;
     private int width , height;
-    private int top;
+    private int radius = 10;
     private static String TAG = CustomView.class.getName();
     public CustomView(Context context) {
         this(context, null);
@@ -55,11 +56,30 @@ public class CustomView extends View {
         paint.setStyle(Paint.Style.STROKE);
         // 画笔宽度
         paint.setStrokeWidth(10);
+//        handler.obtainMessage().sendToTarget();
+        handler.sendEmptyMessageDelayed(1, 60);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(width / 2, height / 2, 200, paint);
+        canvas.drawCircle(width / 2, height / 2, radius, paint);
     }
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if(radius <= 200){
+                radius += 10;
+            }else{
+                radius = 10;
+            }
+            // 每500毫秒发送一个消息
+            Log.d(TAG , "radius : " + radius);
+            // 半径改变后 调用invalidate(); 重新绘制
+            invalidate();
+            handler.sendEmptyMessageDelayed(1 , 60);
+            super.handleMessage(msg);
+        }
+    };
 }
+
